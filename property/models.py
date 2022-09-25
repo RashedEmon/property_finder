b0bd44797a72305945ad5636f00acdf8643c420d
Rashed <rashedulb13@gmail.com>
2022-09-25 23:57:24 +0600

    all working

--- property/models.py
+++ property/models.py
@@ -2,18 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+
 class Location(models.Model):
-    name= models.CharField(max_length=500)
+    name = models.CharField(max_length=500)
 
     def __str__(self) -> str:
         return str(self.name)
+
 
 class Property(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(name='description')
     price = models.FloatField()
-    location = models.ForeignKey(Location,on_delete=models.CASCADE)
-
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return str(self.title)
@@ -24,9 +25,9 @@ class Image(models.Model):
         ('br', 'Bedroom'),
         ('lr', 'Living room')
     ]
-    image=models.ImageField(upload_to = "img/property/")
+    image = models.ImageField(upload_to="img/property/")
     type = models.CharField(max_length=50, choices=CHOICES)
     property = models.ForeignKey(Property, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return str(self.type)
+        return str(self.image)
--- property/views.py
+++ property/views.py
@@ -1,24 +1,51 @@
+from email.mime import image
 from django.shortcuts import render
 from django.http.response import HttpResponse
-from .models import Location, Property
+from .models import Image, Location, Property
+from django.core.paginator import Paginator
 # Create your views here.
-def search(request):
-    range = request.GET.get('range',None)
-    query = request.GET.get('search',None)
-    sorting = request.GET.get('sorting',None)
 
+
+def search(request):
+    range = request.GET.get('range', None)
+    query = request.GET.get('search', '')
+    sorting = request.GET.get('sorting', None)
+    curr_page = request.GET.get('page', 1)
     if sorting == 'htl':
         sorting = '-'
     else:
         sorting = ''
-    
-    res=Property.objects.filter(location__name__icontains=query, price__gt=int(range)).order_by(f'{sorting}price')
+
+    try:
+        range = int(range)
+    except:
+        range = 0
+
+    res = Property.objects.filter(location__name__icontains=query, price__gt=int(
+        range)).order_by(f'{sorting}price')
+    # print(res)
+    card = []
     for property in res.values():
-        print(property)
-    print(res)
-    return render(request,'index.html',content_type="text/html",context={'properties':res})
+        img = Image.objects.filter(property__id=property['id'])[:1]
+        property['image'] = str(img[0])
+        card.append(property)
+
+    p = Paginator(card, 3)
+    page = p.get_page(int(curr_page))
+    return render(request, 'index.html', content_type="text/html", context={'properties': page.object_list,'page':page,'paginator': p})
 
 
 def home(request):
-    res=Property.objects.all()
-    return render(request,'index.html',content_type="text/html",context={'properties':res})
+    res = Property.objects.all()
+
+    card = []
+    for property in res.values():
+        img = Image.objects.filter(property__id=property['id'])[:1]
+        property['image'] = str(img[0])
+        card.append(property)
+    p = Paginator(card, 3)
+    # print(p.object_list)
+    for i in p.page_range:
+        print("emon")
+    page = p.page(1)
+    return render(request, 'index.html', content_type="text/html", context={'properties': page.object_list, 'paginator': p, 'page': page})
